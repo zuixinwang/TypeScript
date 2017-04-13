37c6ef7c8255f779fa6fbf60d94a759201fee36d
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-04-13 13:10:45 -0700

    Add more test cases for simplified typeToString

--- tests/cases/compiler/simpleJsTypes.ts
+++ tests/cases/compiler/simpleJsTypes.ts
@@ -1,14 +1,25 @@
 // @allowJs: true
 // @Filename: whatever.d.ts
 // @noEmit: true
-export interface Cool {
-    a: number;
+export class Cool {
+    spot: number;
 }
-export function view<T>(t: T): T;
+export class World {
+    marvelous: string;
+}
+export function dit(smth: number | '0' | '1' | '2'): Cool & World;
+export function keys<T>(t: T): keyof T;
+export function single<T, K extends keyof T>(t: T, k: K): T[K];
+// TODO: Index types, indexed access types, unions, intersections
+export function lift<T>(t: T): T[];
 
 // @Filename: simpleJsTypes.js
-import { view } from './whatever'
-view({
+import { view, Cool, World, dit, keys, single } from './whatever'
+lift({
     a: 12,
-    m() { return this.a + 1 }
+    m() { }
 });
+keys(new Cool());
+single(new World(), 'marvelous');
+dit(101);
+
