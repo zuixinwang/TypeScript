9d1037776e4db719f98fb76643831d37a4d8ed27
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-07-13 10:09:06 -0700

    Update baselines

---
+++ tests/baselines/reference/jsdocInTypescript.js
@@ -0,0 +1,42 @@
+//// [jsdocInTypescript.ts]
+// grammar error from checker
+var ara: Array.<number> = [1,2,3];
+
+function f(x: ?number, y: Array.<number>) {
+    return x ? x + y[1] : y[0];
+}
+function hof(ctor: function(new: number, string)) {
+    return new ctor('hi');
+}
+function hof2(f: function(this: number, string): string) {
+    return f(12, 'hullo');
+}
+var whatevs: * = 1001;
+var ques: ? = 'what';
+var g: function(number, number): number = (n,m) => n + m;
+var variadic: ...boolean = [true, false, true];
+var most: !string = 'definite';
+var weird1: new:string = {};
+var weird2: this:string = {};
+
+
+//// [jsdocInTypescript.js]
+"use strict";
+// grammar error from checker
+var ara = [1, 2, 3];
+function f(x, y) {
+    return x ? x + y[1] : y[0];
+}
+function hof(ctor) {
+    return new ctor('hi');
+}
+function hof2(f) {
+    return f(12, 'hullo');
+}
+var whatevs = 1001;
+var ques = 'what';
+var g = function (n, m) { return n + m; };
+var variadic = [true, false, true];
+var most = 'definite';
+var weird1 = {};
+var weird2 = {};
---
+++ tests/baselines/reference/jsdocInTypescript2.js
@@ -0,0 +1,16 @@
+//// [jsdocInTypescript2.ts]
+// parse error (blocks grammar errors from checker)
+function parse1(n: number=) { }
+function parse2(n: number!) { }
+function parse3(n: number?) { }
+
+
+//// [jsdocInTypescript2.js]
+// parse error (blocks grammar errors from checker)
+function parse1(n) {
+    if (n === void 0) { n = ; }
+}
+function parse2(n) {
+    if (n === void 0) { n = !; }
+}
+function parse3(n) { }
