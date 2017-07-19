c12a617f46e6f1358e560da94fdf0fe1907a4138
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-07-19 14:41:34 -0700

    Add more test cases

--- tests/cases/conformance/jsdoc/jsdocParamTagTypeLiteral.ts
+++ tests/cases/conformance/jsdoc/jsdocParamTagTypeLiteral.ts
@@ -1,15 +1,24 @@
 // @allowJS: true
 // @checkJs: true
 // @noEmit: true
+// @strict: true
 // @suppressOutputPathCheck: true
 
 // @Filename: 0.js
 /**
+ * @param {Object} notSpecial
+ */
+function normal(notSpecial) {
+    normal; // should just be 'any'
+}
+normal(12);
+
+/**
  * @param {Object} opts
- * @param {string} x
- * @param {string=} y
- * @param {string} [z]
- * @param {string} [w="hi"]
+ * @param {string} opts.x
+ * @param {string=} opts.y
+ * @param {string} [opts.z]
+ * @param {string} [opts.w="hi"]
  */
 function foo(opts) {
     opts.x;
@@ -17,25 +26,36 @@ function foo(opts) {
 
 foo({x: 'abc'});
 
-/**
- * @param {Object} opts
- * @param anotherX {string}
- * @param anotherY {string=}
+/*
+ * @param {Object[]} opts
+ * @param {string} opts[].anotherX
+ * @param {string=} opts[].anotherY
  */
-function foo1(opts) {
-    opts.anotherX;
-}
+//function foo1(/** @param opts bad idea theatre! */opts) {
+    //opts[0].anotherX;
+//}
 
-foo1({anotherX: "world"});
+//foo1([{anotherX: "world"}]);
 
-/**
+/*
  * @param {object} opts
  * @param {string} x
- * @param {string=} y
- * @param {string} [z]
- * @param {string} [w="hi"]
  */
-function foo2(opts) {
-    opts.x;
-}
-foo2({x: 'abc'});
+//function foo2(opts) {
+    //opts.x;
+//}
+//foo2({x: 'abc'});
+
+/*
+ * @param {object[]} opts
+ * @param {string} opts[].x
+ * @param {string=} opts[].y
+ * @param {string} [opts[].z]
+ * @param {string} [opts[].w="hi"]
+ */
+//function foo3(opts) {
+    //opts.x;
+//}
+
+// TODO: Support multiple nesting levels?
+// PROBABLY SO
