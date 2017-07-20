076970219507cb719635ac1a5a97fa9009476498
Nathan Shively-Sanders <nathansa@microsoft.com>
2017-07-20 14:35:05 -0700

    Test:postfix[] jsdoc type literal syntax

--- tests/cases/conformance/jsdoc/jsdocParamTagTypeLiteral.ts
+++ tests/cases/conformance/jsdoc/jsdocParamTagTypeLiteral.ts
@@ -7,18 +7,19 @@
 // @Filename: 0.js
 /**
  * @param {Object} notSpecial
+ * @param {string} unrelated - not actually related because it's not notSpecial.unrelated
  */
 function normal(notSpecial) {
-    normal; // should just be 'any'
+    notSpecial; // should just be 'any'
 }
-normal(12);
+//normal(12);
 
 /**
- * @param {Object} opts
- * @param {string} opts.x
- * @param {string=} opts.y
- * @param {string} [opts.z]
- * @param {string} [opts.w="hi"]
+ * @param {Object} opts doc1
+ * @param {string} opts.x doc2
+ * @param {string=} opts.y doc3
+ * @param {string} [opts.z] doc4
+ * @param {string} [opts.w="hi"] doc5
  */
 function foo(opts) {
     opts.x;
@@ -26,36 +27,36 @@ function foo(opts) {
 
 foo({x: 'abc'});
 
-/*
+/**
  * @param {Object[]} opts
  * @param {string} opts[].anotherX
  * @param {string=} opts[].anotherY
  */
-//function foo1(/** @param opts bad idea theatre! */opts) {
-    //opts[0].anotherX;
-//}
+function foo1(/** @param opts bad idea theatre! */opts) {
+    opts[0].anotherX;
+}
 
-//foo1([{anotherX: "world"}]);
+foo1([{anotherX: "world"}]);
 
-/*
+/**
  * @param {object} opts
- * @param {string} x
+ * @param {string} opts.x
  */
-//function foo2(opts) {
-    //opts.x;
-//}
-//foo2({x: 'abc'});
+function foo2(opts) {
+    opts.x;
+}
+foo2({x: 'abc'});
 
-/*
+/**
  * @param {object[]} opts
  * @param {string} opts[].x
  * @param {string=} opts[].y
  * @param {string} [opts[].z]
  * @param {string} [opts[].w="hi"]
  */
-//function foo3(opts) {
-    //opts.x;
-//}
+function foo3(opts) {
+    opts[0].x;
+}
 
 // TODO: Support multiple nesting levels?
 // PROBABLY SO
