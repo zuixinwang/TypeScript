a471e2ce88e824fe8ed6b73ab9b2a8bc6e648d54
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-01-08 16:20:24 -0800

    Initial tuple spread tests

---
+++ tests/cases/conformance/types/tuple/tupleSpreadCall.ts
@@ -0,0 +1,16 @@
+declare let singleton: [number]
+declare let pair: [number, number]
+declare let wpair: [number, string]
+declare let triple: [number, number, number]
+declare function one(n: number): void;
+declare function two(n: number, m: number): void;
+declare function three(n: number, m: number, total: number): void;
+
+two(...pair)
+two(...wpair)
+three(1, ...pair)
+three(1, ...wpair)
+// syntatically disallowed too early
+three(...pair, 1)
+three(...wpair, 1)
+
