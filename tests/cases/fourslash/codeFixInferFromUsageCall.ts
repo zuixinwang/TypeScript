647537ec386c61072ed75f0b0926e9c371c4d519
Nathan Shively-Sanders <293473+sandersn@users.noreply.github.com>
2018-07-24 10:26:26 -0700

    Just remove the fourslash tests for now

--- tests/cases/fourslash/codeFixInferFromUsageCall.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='fourslash.ts' />
-
-// @noImplicitAny: true
-////function wat([|b |]) {
-////    b();
-////}
-
-// Handled by try-harder-typing
-// verify.rangeAfterCodeFix("b: () => void");
--- tests/cases/fourslash/codeFixInferFromUsageOptionalParam2.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path='fourslash.ts' />
-
-// @noImplicitAny: true
-////function f([|a? |]){
-////    if (a < 9) return;
-////}
-
-// Handled by try-harder-typing
-// verify.rangeAfterCodeFix("a?: number");
